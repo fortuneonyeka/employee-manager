8b23978e4d1c6941e9c04bc2b56afdab1384de99
fortunatus <ihedfortune@gmail.com>
2023-11-28 13:30:21 +0100

    built login page

--- employee-manager/src/components/login/Login.jsx
+++ employee-manager/src/components/login/Login.jsx
@@ -1,7 +1,33 @@
-
+import "../style.css"
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className="h-screen loginPage">
+      <div className="flex flex-col justify-center items-center h-screen w-full  loginForm px-4 ">
+            
+            
+
+<form className="max-w-sm mx-auto  py-24 px-6 w-full border-2 border-gray-400 rounded-xl">
+<h2 className="text-center text-2xl mb-5">Login</h2>
+  <div className="mb-5">
+    <label htmlFor="email" className="block mb-2 text-lg font-medium text-white ">Email:</label>
+    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Email" required />
+    {/* <p className="mt-2 text-sm text-green-600 dark:text-green-500"><span className="font-medium">Alright!</span> Username available!</p> */}
+  </div>
+  <div className="mb-5">
+    <label htmlFor="password" className="block mb-2 textlg font-medium text-white" >Password:</label>
+    <input type="password" name="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-200 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter Your Password" required />
+  </div>
+  <div className="flex items-start mb-5">
+    <div className="flex items-center h-5">
+      <input id="remember" name="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+    </div>
+    <label htmlFor="remember" className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
+  </div>
+  <button type="submit" className="text-white bg-blue-700 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:focus:ring-blue-800 hover:uppercase">Log In</button>
+</form>
+
+      </div>
+    </div>
   )
 }
 
